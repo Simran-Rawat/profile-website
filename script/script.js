ae1bc23c6b2efe17b7a0b15fec78ba7de7c54f43
Simran Rawat <simmujas20@gmail.com>
2022-10-05 12:24:12 +0530

    done

--- script/script.js
+++ script/script.js
@@ -5,6 +5,7 @@ const allLinks = document.querySelectorAll("a:link");
 
 allLinks.forEach(function (link) {
     link.addEventListener("click", function (e) {
+        if (this.classList.contains("no-default")) return;
         e.preventDefault();
         const href = link.getAttribute("href");
 
@@ -26,3 +27,41 @@ allLinks.forEach(function (link) {
             headerEl.classList.toggle("nav-open");
     });
 });
+// whatsapp link changes :)
+
+var isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return (
+            navigator.userAgent.match(/IEMobile/i) ||
+            navigator.userAgent.match(/WPDesktop/i)
+        );
+    },
+    any: function () {
+        return (
+            isMobile.Android() ||
+            isMobile.BlackBerry() ||
+            isMobile.iOS() ||
+            isMobile.Opera() ||
+            isMobile.Windows()
+        );
+    },
+};
+
+if (isMobile.any()) {
+    let whatsApp = document.querySelectorAll(".wa-wa");
+    whatsApp.forEach(function (e) {
+        e.setAttribute("href", "https://wa.me/917534083907/?text=Hi Simran :)");
+    });
+}
